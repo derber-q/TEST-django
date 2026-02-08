b664f5c68f140d4b5b57cb02bc4595b9aaae6368
derber <156999888+derber-q@users.noreply.github.com>
2026-02-08 22:18:29 +0300

    Добавить комментарии к полям модели Product

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 
+from shopapp.models import Product, ProductAdditionalPhoto
 
-# Register your models here.
+
+class ProductAdditionalPhotoInline(admin.TabularInline):
+    model = ProductAdditionalPhoto
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "quantity_remaining", "quantity_unit", "active_discount", "is_archived")
+    list_filter = ("quantity_unit", "is_archived")
+    search_fields = ("name", "description")
+    inlines = [ProductAdditionalPhotoInline]
+
+
+@admin.register(ProductAdditionalPhoto)
+class ProductAdditionalPhotoAdmin(admin.ModelAdmin):
+    list_display = ("product", "id")
+    search_fields = ("product__name",)
